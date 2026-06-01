a00a36a6212908416d16909ac5d95ff7ed0db8ec
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-01 16:48:49 +0500

    Update main.go

--- back/main.go
+++ back/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"log"
+	"os"
 )
 
 type PageData struct {
@@ -10,12 +12,17 @@ type PageData struct {
 }
 
 func renderHtml(w http.ResponseWriter, r *http.Request) {
+	if _, err := os.Stat("../front/index.html"); os.IsNotExist(err) {
+		log.Fatal("файл index.html не найден")
+	}
+	
 	tmpl, err := template.ParseFiles("../front/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	data := PageData { Name: "OpenDelta" }
 	tmpl.Execute(w, data)
 }
